3da6a28c3fd2b9aa27dc99bcd566b561fc3fa379
Alen Rubilar <lclc.alen@gmail.com>
2024-11-23 19:14:29 -0300

    rename routes

--- packages/client/app/self-evaluation/[evaluationId]/page.tsx
+++ packages/client/app/self-evaluation/[evaluationId]/page.tsx
@@ -1,31 +1,33 @@
-"use client";
-//
 import Image from "next/image";
-import { Button } from '@nextui-org/button'; 
-import { ThumbsUp, ThumbsDown } from 'lucide-react';
-//
-import avatar from "../assets/avatar.png";
+import { Button } from "@nextui-org/button";
+import { ThumbsUp, ThumbsDown } from "lucide-react";
+
+import avatar from "@/assets/avatar.png";
 import layout from "./page.module.scss";
 
-const CardsPage = () => {
+export default async function CardsPage({
+  params,
+}: {
+  params: Promise<{ evaluationId: string }>;
+}) {
+  const { evaluationId } = await params;
+
   return (
     <main className={layout.main}>
       <section className={layout.wrapper}>
         <div className={layout.card}>
           <div>
             <Image src={avatar} alt="avatar" height={65} width={60} />
-            <p>William</p>
+            <p>William ({evaluationId})</p>
           </div>
-          <h1>
-            ¿Está experimentando dificultad para respirar?
-          </h1>
+          <h1>¿Está experimentando dificultad para respirar?</h1>
           <div>
             <Button size="lg">
-              <ThumbsUp className="mr-1 h-6 w-6" />
+              <ThumbsUp className="w-6 h-6 mr-1" />
               Sí
             </Button>
             <Button size="lg">
-              <ThumbsDown className="mr-1 h-6 w-6" />
+              <ThumbsDown className="w-6 h-6 mr-1" />
               No
             </Button>
           </div>
@@ -34,5 +36,3 @@ const CardsPage = () => {
     </main>
   );
 }
-
-export default CardsPage;
