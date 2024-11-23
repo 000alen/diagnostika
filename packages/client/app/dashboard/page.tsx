6b7dfd1a7124cdde267e8afbdb58a6003c80a9f5
CxrlosKenobi <cxrlos@kenobi.dev>
2024-11-23 15:45:15 -0300

    feat(client): add Dropdown component, update styles, and enhanced Settings tab
    Co-authored-by: Sebastián Torrealba <storrealbac@users.noreply.github.com>

--- packages/client/app/dashboard/page.tsx
+++ packages/client/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 //
 import Image from "next/image";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/react";
 import { Tabs, Tab } from "@nextui-org/tabs";
 import { Button } from "@nextui-org/button";
 import { Badge } from "@nextui-org/badge";
@@ -9,61 +10,15 @@ import { Bell } from 'lucide-react';
 import OverviewTab from "../components/tabs/overview";
 import PatientsTab from "../components/tabs/patients";
 import DiagnosticsTab from "../components/tabs/diagnostics";
+import SettingsTab from "../components/tabs/settings";
 import avatar from "../assets/avatar.png";
 import layout from "./page.module.scss";
-
-import {Input} from "@nextui-org/input";
-
-const SettingsTab = () => {
-  return (
-    <div className={layout.settingsTab}>
-      <h2>Account Settings</h2>
-      <div className={layout.card}>
-        <form className={layout.form}>
-          <div className={layout.formGroup}>
-          <Input
-            type="email"
-            label="Email"
-            defaultValue="cxrlos@kenobi.dev"
-            className="max-w-xs"
-          />
-          </div>
-          <div className={layout.formGroup}>
-          <Input
-            type="email"
-            label="Email"
-            defaultValue="cxrlos@kenobi.dev"
-            className="max-w-xs"
-          />
-          </div>
-          <div className={layout.formGroup}>
-            <label htmlFor="birthdate">Date of Birth</label>
-            <Input id="birthdate" type="date" />
-          </div>
-          <div className={layout.formGroup}>
-            <label htmlFor="email">Email</label>
-            <Input id="email" type="email" placeholder="Enter your email" />
-          </div>
-          <div className={layout.formButtons}>
-            <Button color="primary" radius="full">
-              Save Changes
-            </Button>
-            <Button color="secondary" radius="full" variant="ghost">
-              Reset
-            </Button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-};
-
 
 const tabs = [
   { name: "Overview", content: <OverviewTab /> },
   { name: "Patients", content: <PatientsTab /> },
   { name: "Diagnostics", content: <DiagnosticsTab /> },
-  { name: "Settings", content: <SettingsTab/> },
+  { name: "Settings", content: <SettingsTab/> }
 ];
 
 const ReviewerDashboard = () => {
@@ -74,17 +29,30 @@ const ReviewerDashboard = () => {
           <h1>ProHealth</h1>
         </div>
         <div>
-          <Button radius="full" isIconOnly>
+          <Button radius="full" isIconOnly className="h-[60px] w-[60px] bg-white">
             <Badge color="danger" shape="circle" content={1}>
               <Bell color="#1e1e1e" />
             </Badge>
           </Button>
-          <Image
-            src={avatar}
-            alt="Avatar"
-            width={60}
-            height={60}
-          />
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Image
+                src={avatar}
+                alt="Avatar"
+                width={60}
+                height={60}
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile actions" variant="flat">
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">william@hospital.cl</p>
+              </DropdownItem>
+              <DropdownItem key="logout" color="danger">
+                Logout
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </div>
       </header>
       <Tabs aria-label="Reviewer Dashboard" className={layout.tabs}  radius="full" color="white">
