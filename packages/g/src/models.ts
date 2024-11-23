67347d977ad59ff389760034d67a444696709a8a
Alen Rubilar <lclc.alen@gmail.com>
2024-11-23 05:36:07 -0300

    parse testing data

--- packages/g/src/models.ts
+++ packages/g/src/models.ts
@@ -1,10 +1,10 @@
 import { openai } from "@ai-sdk/openai";
-// import { bedrock } from "@ai-sdk/amazon-bedrock";
+import { bedrock } from "@ai-sdk/amazon-bedrock";
 
 export const gpt4o = openai("gpt-4o");
 
 export const openaiEmbeddings = openai.embedding("text-embedding-3-large");
 
-// export const sonnet = bedrock("anthropic.claude-3-5-sonnet-20241022-v2:0");
+export const sonnet = bedrock("anthropic.claude-3-sonnet-20240229-v1:0");
 
-// export const cohereEmbeddings = bedrock.embedding("cohere.embed-english-v3");
+export const cohereEmbeddings = bedrock.embedding("cohere.embed-english-v3");
