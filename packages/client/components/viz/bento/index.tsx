a05e1e4178e3856d1dafc9b9ade3542582c1de32
CxrlosKenobi <cxrlos@kenobi.dev>
2024-11-23 22:37:26 -0300

    feat(client): :sparkles: enhance BentoGrid component with new grid card types and layout adjustments

--- packages/client/components/viz/bento/index.tsx
+++ packages/client/components/viz/bento/index.tsx
@@ -40,11 +40,45 @@ export const BentoGridSide = ({ setActiveTab }: Props) => {
           <ArrowIcon />
         </Button>
       </div>
-      <div className={layout.customGrid}>
-        <div className={layout.gridCard}>
+      <div className={layout.gridRow} style={{ marginBottom: 24}}>
+        <div className={layout.gridCardTypeD}>
+        </div>
+        <div className={layout.gridCardTypeC}>
+          <Button size="lg" radius="full" isIconOnly>
+            <ArrowIcon />
+          </Button>
+          <span>Scan <br/>Cardiology</span>
+          <p>1,276</p>
+        </div>
+      </div>
+      <div className={layout.gridRow}>
+        <div className={layout.gridCardTypeA}>
           <div>
             <Image src={avatar} alt="avatar" height={35} width={45} />
             <p>William ({1})</p>
+          </div>
+          <div className={layout.biometric}>
+            <p>Heart Rate</p>
+            <span>110 bpm</span>
+          </div>
+          <div className={layout.biometric}>
+            <p>Temperatura</p>
+            <span>34.7°C</span>
+          </div>
+          <div className={layout.biometric}>
+            <p>SpO2</p>
+            <span>96%</span>
+          </div>
+          <Button size="lg" radius="full" isIconOnly onClick={goToPerfilPaciente}>
+            <ArrowIcon />
+          </Button>
+        </div>
+        <div className={layout.gridCardTypeB}>
+          <div>
+            <div className="flex items-center w-fit px-3 py-1 mb-1 rounded-full backdrop-filter backdrop-blur-sm bg-white/15 text-sm font-light select-none border-1 border-[#8D959D] tracking-wider">
+              87% probable
+            </div>
+            <span>Osteoporosis</span>
           </div>
           <Button size="lg" radius="full" isIconOnly onClick={goToPerfilPaciente}>
             <ArrowIcon />
